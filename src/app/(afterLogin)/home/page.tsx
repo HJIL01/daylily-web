cebd449a723ecf28aecfccdb731ee8ffd21b0c76
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2025-08-16 00:21:20 +0900

    feat: ✨ home 퍼블리싱

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import MainLogo from "@/assets/icons/MainLogo";
 import clsx from "clsx";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,13 +20,13 @@ export default function Home() {
           "rounded-4xl",
           "bg-white",
           "max-md:w-[90dvw]",
-          "shadow-xl",
+          "shadow-xl shadow-black/10",
         )}
       >
         <div
           className={clsx(
             "flex flex-col items-center",
-            "px-[5%] py-17",
+            "py-17",
             "h-[98%] w-[98%]",
             "bg-daylily-linear-200/50",
             "rounded-[2.5rem]",
@@ -32,20 +35,93 @@ export default function Home() {
           <div className="mb-14 w-[7vw] min-w-50">
             <MainLogo />
           </div>
-          <div className="w-full">
-            <h3 className="mb-9 text-2xl font-bold text-black">
+
+          <div className="h-full w-full">
+            <h3
+              className={clsx(
+                "px-[7%] text-2xl font-bold text-black",
+                "max-md:text-xl max-sm:text-lg",
+              )}
+            >
               Imported Git Repository
             </h3>
             <ul
               className={clsx(
                 "grid grid-cols-2 gap-x-5 gap-y-7",
-                "max-h-200 overflow-y-auto",
+                "px-[5%] py-9",
+                "max-h-4/5 overflow-y-auto",
                 "scrollbar-hidden",
+                "max-md:grid-cols-1",
               )}
             >
-              {Array.from({ length: 28 }, (_, i) => i).map((e) => (
-                <li key={e} className="h-60 rounded-3xl bg-white">
-                  카드{e + 1}
+              {Array.from({ length: 25 }, (_, i) => i).map((e) => (
+                <li
+                  key={e}
+                  className={clsx(
+                    "flex items-center justify-center",
+                    "rounded-3xl bg-white",
+                    "shadow-primary/5 shadow-lg",
+                    "transition-all duration-200",
+                    "hover:shadow-primary/15 hover:shadow-lg",
+                    "hover:-translate-y-2",
+                    "cursor-pointer",
+                  )}
+                >
+                  <div
+                    className={clsx(
+                      "h-[96%] w-[98%]",
+                      "rounded-[1.6rem]",
+                      "bg-daylily-linear-200/10",
+                      "p-6",
+                    )}
+                  >
+                    <div className="mb-6 flex gap-6">
+                      <div
+                        className={clsx(
+                          "h-16 w-16 shrink-0 rounded-full",
+                          "bg-gray-200",
+                        )}
+                      ></div>
+
+                      <div className="flex min-w-0 flex-col">
+                        <h4 className="text-xs font-semibold text-gray-600">
+                          프로젝트 이름
+                        </h4>
+                        <Link
+                          href={"/home"}
+                          className={clsx(
+                            "block w-full truncate",
+                            "text-xxs text-gray-500",
+                            "hover:!underline",
+                          )}
+                        >
+                          daylily도커컨테이너asdasdadsaurl.com
+                        </Link>
+                      </div>
+                    </div>
+
+                    <Link
+                      href={"/home"}
+                      className={clsx(
+                        "inline-block",
+                        "rounded-[10rem]",
+                        "bg-daylily-linear-400 text-primary",
+                        "mb-3 px-4 py-3",
+                        "text-sm font-medium",
+                        "hover:!underline",
+                      )}
+                    >
+                      Daylily-web/main
+                    </Link>
+                    <span
+                      className={clsx(
+                        "block",
+                        "text-xs font-medium text-gray-300",
+                      )}
+                    >
+                      Mar 29 on - main
+                    </span>
+                  </div>
                 </li>
               ))}
             </ul>
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -16,7 +16,7 @@ export default function BeforeLoginMain() {
         )}
       >
         <div
-          className={clsx("mb-14 w-full", "animate-fade-in-first opacity-0")}
+          className={clsx("mb-14 w-full", "animate-fade-in-daylily opacity-0")}
         >
           <MainLogo />
         </div>
@@ -26,7 +26,7 @@ export default function BeforeLoginMain() {
             "text-2xl text-gray-400",
             "mb-54",
             "font-medium",
-            "animate-fade-in-second opacity-0",
+            "animate-fade-in-intro opacity-0",
             "max-md:text-lg",
             "max-sm:text-base",
           )}
@@ -40,15 +40,16 @@ export default function BeforeLoginMain() {
           className={clsx(
             "flex items-center justify-center gap-5",
             "border-daylily-black rounded-lg border",
-            "w-full py-5",
-            "text-base font-bold",
-            "animate-fade-in-third opacity-0",
+            "px-20 py-5",
+            "font-bold",
+            "animate-fade-in-login-btn opacity-0",
+            "text-base max-md:text-sm max-sm:text-xs",
           )}
         >
           <span>
             <Github width={26} />
           </span>
-          <span>Continue With Github</span>
+          <span className="whitespace-nowrap">Continue With Github</span>
         </Link>
       </div>
 
@@ -58,10 +59,10 @@ export default function BeforeLoginMain() {
           "absolute right-0 bottom-0 z-10",
           "h-auto w-[55dvw] min-w-200",
           "opacity-0",
-          "animate-fade-in-first",
+          "animate-fade-in-flowers",
         )}
       >
-        <Image src={GlassFlowers} alt="Glass Flowers" />
+        <Image src={GlassFlowers} alt="Glass Flowers" priority />
       </div>
     </div>
   );
